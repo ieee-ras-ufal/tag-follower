64e07061f1ae2d5e59385f858be16f63a1a67b9f
glauberrleite <~contact@glauberrleite.com~>
2019-10-03 18:42:14 -0300

    working prototype

--- src/follower.py
+++ src/follower.py
@@ -5,18 +5,16 @@ import numpy
 from tf.msg import tfMessage
 from Driver import Driver
 
-Ts = 0.1
+Ts = .1
 
 class Follower:
 
     def __cb_tag(self, msg):
-        tag = numpy.array([msg.transforms.transform.translation.x, msg.transforms.transform.translation.y, msg.transforms.transform.translation.z])
+        tag = numpy.array([msg.transforms[0].transform.translation.x, msg.transforms[0].transform.translation.y, msg.transforms[0].transform.translation.z])
         self.tag_position = self.__to_origin(tag)
 
     def __to_origin(self, position):
-        T_E_0 = numpy.linalg.inv(self.fkm())
-
-        result = numpy.dot(T_E_0, numpy.append(position, 1))
+        result = numpy.dot(self.fkm(), numpy.append(position, 1))
 
         return result[0:3]
 
@@ -25,10 +23,20 @@ class Follower:
             theta_1 = self.theta[0]
         if (theta_2 == None):
             theta_2 = self.theta[1]
-        
-        kine = self.fkm(theta_1, theta_2)
 
-        return kine[0:3, 3]
+	L1 = self.L[0]
+	L2 = self.L[1]
+	L3 = self.L[2]
+	L4 = self.L[3]
+
+	s1 = numpy.sin(theta_1)
+	s2 = numpy.sin(theta_2)
+	c1 = numpy.cos(theta_1)
+	c2 = numpy.cos(theta_2)
+        
+        kine = numpy.array([L2*c1 - L3*c1*s2 + L4*c1*c2, L2*s1 - L3*s1*s2 + L4*s1*c2, L1 + L3*c2 + L4*s2])
+
+        return kine
 
     def fkm(self, theta_1 = None, theta_2 = None):
         if (theta_1 == None):
@@ -46,9 +54,9 @@ class Follower:
         L3 = self.L[2]
         L4 = self.L[3]
 
-        T_0_E = numpy.array([[c1*s2, -s1, c1*c2, L2*c1 + L3*c1*c2 - L4*c1*s2],
-        [s1*s2, c1, s1*c2, L2*s1 + L3*s1*c2 - L4*s1*s2],
-        [-c2, 0, s2, L1 + L3*s2 + L4*c2],
+        T_0_E = numpy.array([[s1, c1*s2, c1*c2, L2*c1 - L3*c1*s2 + L4*c1*c2],
+        [-c1, s1*s2, s1*c2, L2*s1 - L3*s1*s2 + L4*s1*c2],
+        [0, -c2, s2, L1 + L3*c2 + L4*s2],
         [0, 0, 0, 1]])
 
         return T_0_E
@@ -68,18 +76,18 @@ class Follower:
         L3 = self.L[2]
         L4 = self.L[3]
 
-        J = numpy.array([[-L2*s1 - L3*s1*c2 + L4*s1*s2, -L3*c1*s2 - L4*c1*c2],
-        [L2*c1 + L3*c1*c2 - L4*c1*s2, -L3*s1*s2 -L4*s1*c2],
-        [0, L3*c2 - L4*s2]])
+        J = numpy.array([[-L2*s1 +  L3*s1*s2 - L4*s1*c2, -L3*c1*c2 - L4*c1*s2],
+        [L2*c1 - L3*c1*s2 + L4*c1*c2, -L3*s1*c2 -L4*s1*s2],
+        [0, -L3*s2 + L4*c2]])
 
         return J
 
     def __init__(self):
         rospy.init_node('follower')
-        rospy.Subscriber('/ar_pose_marker', tfMessage, self.__cb_tag)
+        rospy.Subscriber('/camera_cube_tf', tfMessage, self.__cb_tag)
         self.theta = numpy.zeros(2)
         self.L = numpy.array([0.093, 0.025, 0.0257, 0.095])
-        self.tag_position = self.position()
+	self.tag_position = self.position()
 
 follower = Follower()
 
@@ -91,12 +99,20 @@ def shutdown_hook():
     driver.closeConn()
 
 # Main loop
+cnt = 0
 rospy.on_shutdown(shutdown_hook)
 rate = rospy.Rate(1/Ts)
 while not rospy.is_shutdown():
 
     # Computing feedback control
-    error = follower.tag_position - follower.position()
+    goal = numpy.copy(follower.tag_position)
+    #goal = numpy.array([1, 0.5, 0.5])
+    goal[2] -= follower.L[0]
+    versor = goal/numpy.linalg.norm(goal)
+    setpoint = (follower.L[1] + follower.L[3]) * versor
+    setpoint[2] += follower.L[0]
+    error = setpoint - follower.position()
+
     dtheta = numpy.dot(numpy.linalg.pinv(follower.jacobian()), error)
 
     # Euler integration
@@ -108,3 +124,8 @@ while not rospy.is_shutdown():
     rate.sleep()
 
     follower.theta = theta
+
+    cnt += 1
+    if (cnt % 10):
+	print(follower.tag_position)
+	cnt = 0
